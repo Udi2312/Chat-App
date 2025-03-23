5c043d6610625b7f107284437f5ffe17a0109d26
Udit Bansal <Udit.ban2312@gmail.com>
2025-03-24 00:37:16 +0530

    first commit

---
+++ server/src/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const Signup = (req, res) => {
+    res.send("Signup route");
+}
+export const Login = (req, res) => {
+    res.send("Login route");
+}
+export const Logout = (req, res) => {
+    res.send("Logout route");
+}
---
+++ server/src/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './lib/db.js';
+dotenv.config();
+import authRoutes from './routes/auth.route.js';
+import { connect } from 'mongoose';
+const app = express();
+
+app.use("/api/auth" , authRoutes)
+const PORT = process.env.PORT ;
+
+app.listen(PORT, () => {
+    console.log('Server is running on PORT: '+ PORT);
+    connectDB();
+});
