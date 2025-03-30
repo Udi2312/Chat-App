ef3efa6cec071684de693abb2759312ee5727689
Udit Bansal <Udit.ban2312@gmail.com>
2025-03-30 21:35:42 +0530

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
-
+import Navbar from './components/Navbar'
+import { Routes , Route} from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import ProfilePage from './pages/ProfilePage'
+import SignupPage from './pages/SignupPage' 
+import SettingsPage from './pages/SettingsPage'
+import { useAuthStore } from './store/authStore'
 const App = () => {
+  const { authUser, checkAuth } = useAuthStore()
   return (
-    <div className='text-red-500'>Hello World</div>
+    <div>
+   <Navbar />
+   
+   <Routes>
+    <Route path='/' element={<HomePage />} />
+    <Route path='/login' element={<LoginPage />} />
+    <Route path='/profile' element={<ProfilePage />} />
+    <Route path='/signup' element={<SignupPage />} />
+    <Route path='/settings' element={<SettingsPage />} />
+   </Routes>
+    </div>
+    
   )
 }
 
