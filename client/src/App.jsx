3ae7546a118e2ea3cbe9ac42cc30cb088d7e57c1
Udit Bansal <Udit.ban2312@gmail.com>
2025-04-10 23:14:54 +0530

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import { Routes , Route} from 'react-router-dom'
+import { Routes , Route, Navigate} from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import ProfilePage from './pages/ProfilePage'
@@ -8,26 +8,35 @@ import SignupPage from './pages/SignupPage'
 import SettingsPage from './pages/SettingsPage'
 import { useAuthStore } from './store/useAuthStore'
 import { useEffect } from 'react'
+import { Loader } from 'lucide-react'
 const App = () => {
-  const { authUser, checkAuth } = useAuthStore()
+  const { authUser, checkAuth , isCheckingAuth} = useAuthStore()
   useEffect(() => {
     checkAuth()
   }, [checkAuth]);
+
+
   console.log({ authUser })
+
+  if(isCheckingAuth && !authUser) {
+    return(
+    <div className='flex justify-center items-center h-screen'>
+      <Loader className='animate-spin size-10' />
+      </div>
+    )
+  }
   return (
     <div>
    <Navbar />
    
    <Routes>
-    <Route path='/' element={<HomePage />} />
-    <Route path='/login' element={<LoginPage />} />
-    <Route path='/profile' element={<ProfilePage />} />
-    <Route path='/signup' element={<SignupPage />} />
+    <Route path='/' element={authUser ? <HomePage /> : <Navigate to='/login' />} />
+    <Route path='/login' element={!authUser ? <LoginPage />: <Navigate to='/' /> } />
+    <Route path='/profile' element={authUser ? <ProfilePage /> : <Navigate to='/login' />} />
+    <Route path='/signup' element={!authUser ? <SignupPage />: <Navigate to='/' /> } />
     <Route path='/settings' element={<SettingsPage />} />
    </Routes>
-    </div>
-    
-  )
+   </div> 
+  ) 
 }
-
 export default App
--- client/src/pages/SignupPage.jsx
+++ client/src/pages/SignupPage.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import {useState} from 'react'
+import { MessageSquare } from 'lucide-react';
+import { useAuthStore } from '../store/useAuthStore'
+
 
 const SignupPage = () => {
+    const [shownPassword, setShowPassword] = useState(false)
+    const [fromData, setFormData] = useState({
+        fullName: "",
+        email: "",
+        password: "",
+    })
+
+    const {signup , isSigningUp} = useAuthStore();
+    const validateForm = () => {}
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
   return (
-    <div>SignupPage</div>
-  )
-}
+    <div className='min-h-screen grid lg:grid-cols-2'>
+       { /* Left Side */}
+       <div className='flex flex-col justify-content items-center p-6 sm:p-12'>
+        <div className="w-full max-w-md space-y-8">
+            {/* Logo */}
+            <div className="text-center mb-8">
+                <div className="flex flex-col items-center gap-2 group">
+                    <div className="size-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover: bg-primary/20 transition-colors">
+                    </div>
+                </div>
+            </div>
+        <MessageSquare />
+
+        {/* {/* {/* <form onSubmit={handleSubmit} className='space-y-6'>
+        < div classname= "form—control"> 
+        <label className='label'
+            <span className=" label—text font—medium">Full Name</span>
+            </label>
+            <div className="relative">
+                <div className="absolute insert-y"></div> */}
+            </div> 
+       </div> 
+    </div>
+  )}
+
 
 export default SignupPage
