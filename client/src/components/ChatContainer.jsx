ea2c756aae5f31f0b8a7836a9dec681d6f07a89b
Udit Bansal <Udit.ban2312@gmail.com>
2025-04-20 11:44:49 +0530

    commit

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import ChatHeader from './skeletons/ChatHeader'
+import MessageInput from './skeletons/MessageInput'
+import MessageSkeleton from './skeletons/MessageSkeleton'
+import { useChatStore } from '../store/useChatStore'
 
 const ChatContainer = () => {
+    const { messages , selectedUser, getMessages, isMessagesLoading } = useChatStore();
+
+
+    useEffect(() => {
+        getMessages(selectedUser._id)
+    },[selectedUser._id, getMessages])
+
+    if(isMessagesLoading) return (
+      <div className='flex-1 flex flex-col overflow-auto'>
+        <ChatHeader />
+        <MessageSkeleton />
+        <MessageInput />
+      </div>
+    )
+
+
   return (
-    <div>ChatContainer</div>
+    <div className='flex-1 flex flex-col overflow-auto'>
+        <ChatHeader />
+
+        <p>messages...</p>
+
+        <MessageInput />
+
+    </div>
   )
 }
 
