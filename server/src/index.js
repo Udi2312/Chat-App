68c9465b4217eba9fc8b5fd1238e560a725c2de2
Udit Bansal <Udit.ban2312@gmail.com>
2025-04-27 23:52:18 +0530

    commit

--- server/src/index.js
+++ server/src/index.js
@@ -5,7 +5,7 @@ import { connectDB } from './lib/db.js';
 dotenv.config();
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
-// import { connect } from 'mongoose';
+import path from 'path';
 import cookieParser from 'cookie-parser';
 import { server , app } from './lib/socket.js';
 
@@ -15,9 +15,18 @@ app.use(cors({
     origin: ["http://localhost:5173"],
     credentials: true,
 }));
+
+const __dirname = path.resolve();
 app.use("/api/auth" , authRoutes)
 app.use("/api/messages" , messageRoutes)
 const PORT = process.env.PORT ;
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../client","dist", "index.html"));
+    });
+}
 
 server.listen(PORT, () => {
     console.log('Server is running on PORT: '+ PORT);
